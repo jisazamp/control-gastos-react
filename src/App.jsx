0c911d81d19090dbfe91ed8753ee468c58ab171d
Juan P. Isaza <jisazamp@gmail.com>
2022-06-13 10:52:07 -0500

    Added the plus SVG

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,25 @@ const App = () => {
           setIsValidBudget={setIsValidBudget}
         />
       )}
+
+      {isValidBudget && (
+        <div
+          className='fixed h-16 w-16 active:h-20 active:w-20 bottom-4 right-4 lg:bottom-8 lg:right-8 fill-sky-400 lg:cursor-pointer lg:hover:h-20 lg:hover:w-20
+      transition-all duration-200 ease-in stroke-white'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+        </div>
+      )}
     </main>
   );
 };
